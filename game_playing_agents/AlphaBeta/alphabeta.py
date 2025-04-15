7a247c01e5582cfd102a939b0a9367c9ff4fe5ee
Sahiti Vempalli <vsahiti92005@gmail.com>
2025-04-15 15:34:59 +0530

    feat(agent): add alpha-beta pruning for chess

---
+++ game_playing_agents/AlphaBeta/alphabeta.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+# Add the path to GameEnv folder (update if needed)
+sys.path.append(os.path.abspath("../GameEnv"))
+
+from GameEnv import GameEnv
+import chess
+
+class AlphaBeta:
+    def __init__(self, max_depth, env: GameEnv):
+        self.max_depth = max_depth
+        self.env = env
+
+    def alpha_beta(self, state, depth, alpha, beta, is_max):
+        board = state.unwrapped._board
+
+        if depth == 0 or self.env.is_terminal(state, depth):
+            return self.env.evaluate(state), None
+
+        legal_moves = list(board.legal_moves)
+
+        if is_max:
+            max_eval = float('-inf')
+            best_action = None
+            for move in legal_moves:
+                board.push(move)
+                eval_score, _ = self.alpha_beta(state, depth - 1, alpha, beta, False)
+                board.pop()
+                if eval_score > max_eval:
+                    max_eval = eval_score
+                    best_action = move
+                alpha = max(alpha, eval_score)
+                if beta <= alpha:
+                    break  # β cutoff
+            return max_eval, best_action
+
+        else:
+            min_eval = float('inf')
+            best_action = None
+            for move in legal_moves:
+                board.push(move)
+                eval_score, _ = self.alpha_beta(state, depth - 1, alpha, beta, True)
+                board.pop()
+                if eval_score < min_eval:
+                    min_eval = eval_score
+                    best_action = move
+                beta = min(beta, eval_score)
+                if beta <= alpha:
+                    break  # α cutoff
+            return min_eval, best_action
+
+    def get_best_action(self, state,is_max):
+        _, best_action = self.alpha_beta(state, self.max_depth, float('-inf'), float('inf'), is_max)
+        return best_action
